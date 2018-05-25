a9dfa4bef70d8a71f53c591e64e370ad8cf05192
vohungvi <vohungvi@vohungvi.com>
2018-05-25 14:57:58 +0700

    + add

---
+++ CrabCounter/WinMain.cpp
@@ -0,0 +1,48 @@
+// CrabCounter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "CrabCounter.h"
+#include <tchar.h>
+#include "TGMTdraw.h"
+#include "TGMTdebugger.h"
+#include "TGMTfile.h"
+#include "TGMTConfig.h"
+#include "TGMTutil.h"
+
+/////////////////////////////////////////////////////////////////////////////
+
+void _tmain(int argc, _TCHAR* argv[])
+{
+	GetTGMTConfig()->LoadSettingFromFile();
+
+	std::string dir = GetTGMTConfig()->ReadValueString("crabcounter", "dir");
+	if (dir.empty())
+		dir = TGMTfile::GetCurrentDir();
+
+	PrintMessage("Read image in dir: %s", dir.c_str());
+
+	std::vector<std::string> images = TGMTfile::GetImageFilesInDir(dir);
+	if (images.size() == 0)
+	{
+		PrintError("Can not load any image in dir: %s", dir.c_str());
+		StopProgram
+	}
+	else
+	{
+		PrintMessage("Loaded %d image", images.size());
+	}
+
+	TGMTfile::CreateDir("output");
+	for (int i = 0; i < images.size(); i++)
+	{
+		cv::Mat mat = cv::imread(images[i]);
+		int count = GetCrabCounter()->CountCrab(mat);
+		std::cout << images[i].c_str() << ": " << count << "\n";
+		
+		WriteImageAsync(mat, "output\\%s_detected.jpg", TGMTfile::GetFileNameWithoutExtension(images[i]).c_str());
+	}
+
+	StopProgram
+}
+
